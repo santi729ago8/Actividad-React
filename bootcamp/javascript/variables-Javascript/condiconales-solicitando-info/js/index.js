865d4d3b957d54f0beb95fe8fdeb4973124f3866
santiago <santimilan_29@hotmail.com>
2023-05-16 14:17:36 -0500

    acutalizacion de la carpeta condicionales

--- bootcamp/javascript/variables-Javascript/condiconales-solicitando-info/js/index.js
+++ bootcamp/javascript/variables-Javascript/condiconales-solicitando-info/js/index.js
@@ -2,25 +2,33 @@
 let datosCompletos = false
 let nombre = prompt("Ingrese su nombre")
 if (nombre != '') {
-    // alert(nombre)
-}
-let apellido = prompt("Ingrese el apellido")
-if (apellido != "") {
-    // alert(apellido)
-}
-let textoEdad = prompt("Ingrese su edad")
-if (textoEdad != '') {
-    let edad = Number(textoEdad)
-    // alert(typeof edad)
-    if (edad >= 18)
-        document.write("Bienvenido a el bar BIT")
-} else {
-    alert("Eres menor de edad No puedes ingresar al sitio")
-} else {
-    alert("Hey la edad es requerido")
+    let apellido = prompt("Ingrese el apellido")
+    if (apellido != "") {
+        // alert(apellido)
+        let textoEdad = prompt("Ingrese su edad")
+        if(textoEdad !=''){
+            let edad = Number(textoEdad)
+            //alert (typeof edad)
+            if (edad >= 18){
+                document.write("Bienvenido a el bar BIT")
+            } else{
+                alert("Eres menor de edad No puedes ingresar al sitio")
+            }
+        }else{
+            alert("Hey la edad es requerido")
+            window.location.reload()
+        }
+        
+    }else{
+        alert("Hey el apellido es requerido")
+        window.location.reload()
+    }
+    
+} else{
+    alert("Hey el nombre es requerido")
     window.location.reload()
-} else {
-    alert("Hey el apellido es requerdio")
-    window.location.reload()
-    //alert (nombre)
 }
+// alert(nombre)
+
+
+
